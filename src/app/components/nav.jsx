e63630247c308344e402b05cf37d163aa2316092
SFNTK <153030492+SFNTK@users.noreply.github.com>
2024-12-05 16:13:41 +0100

    Update nav.jsx

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -9,25 +9,7 @@ import { useState } from 'react'
 
 const Nav = () => {
   const router = useRouter()
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [MenuOMenuiconpen, setMenuicon] = useState(false)
-  const refmenu = useRef()
-  useEffect(() => {
-    if (isMenuOpen) {
-      setMenuicon(<X  className=" iconmenu w-6 h-6" onClick={() => {
-        setIsMenuOpen(false)
-      
-        refmenu.current.style.transform = "translateX(1000px)";
-      }} />)
-      refmenu.current.style.transform="translateX(0px)"
-    }else{
-      setMenuicon(<Menu   onClick={()=>{
-          setIsMenuOpen(true)
-        }}  className={`h-6 w-6 iconmenu transition-transform ${isMenuOpen ? 'rotate-180' : ''}`} />
-      )
-      refmenu.current.style.transform="translateX(1000px)"
-    }
-  }, [isMenuOpen])
+ 
   return (
     <>
       <header id="nav" className="px-4 sticky top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -52,30 +34,7 @@ const Nav = () => {
         </div>
       </header>
 
-      <div id="navphone"
-
-        ref={refmenu}
-
-      >
-       
-        <nav className="container navphoneinside py-4 flex flex-col w-full items-center space-y-2">
-          <a href="#home" className="text-sm font-medium hover:underline bg-darkblue w-full py-2 text-center"
-            onClick={() => {
-setIsMenuOpen(false)            }}>Home</a>
-          <a href="#about" className="text-sm font-medium hover:underline bg-darkblue w-full py-2 text-center" onClick={() => {
-        setIsMenuOpen(false)  
-          }}>About</a>
-          <a onClick={() => {
-       setIsMenuOpen(false)  
-          }} href="#services" className="text-sm font-medium hover:underline bg-darkblue w-full py-2 text-center">Services</a>
-          <a onClick={() => {
-           setIsMenuOpen(false)  
-          }} href="#testimonials" className="text-sm font-medium hover:underline bg-darkblue w-full py-2 text-center">Testimonials</a>
-          <a onClick={() => {
-           setIsMenuOpen(false)  
-          }} href="#contact" className="text-sm font-medium hover:bg-semidark bg-darkblue w-full py-2 text-center" >Contact</a>
-        </nav>
-      </div>
+    
 
     </>
   );
